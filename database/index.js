b217c2599e80d5979e68230990fe1b8cb5d371c8
Haosen Tan <htan68@uwo.ca>
2022-10-30 11:47:25 -0400

    back-end project init

---
+++ database/index.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql')
+
+// 创建数据库连接对象
+const database = mysql.createPool({
+  host: '127.0.0.1',
+  user: 'root',
+  password: '88888888',
+  database: 'lab3_music',
+  multipleStatements: true
+})
+
+// 向外共享 db 数据库连接对象
+module.exports = database
