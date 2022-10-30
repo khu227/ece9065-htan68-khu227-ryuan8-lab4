5c497ee2965bc7e2731958d9108229f1f57f7a76
Haosen Tan <htan68@uwo.ca>
2022-10-30 12:20:30 -0400

    Methods that implement the artistdetails interface

---
+++ router_handler/artistdetails.js
@@ -0,0 +1,28 @@
+const database = require('../database/index')
+//item 2
+exports.getArtistdetails = (req, res) => {
+    const sql = `select artist_id, artist_active_year_begin, artist_favorites, artist_handle, artist_location, artist_name from raw_artists where artist_id=?`
+    database.query(sql,[req.params.artist_id],(err,results) => {
+
+        //to-do err message
+        if (err) return console.log(err.message)
+        // let str = JOSN.stringify(results)
+        // res.send({
+        //     status:200,
+        //     message:'',
+        //     data: results,
+        // })
+        res.send(results)
+    })
+}
+//item 5
+exports.getArtistname = (req,res) => {
+    const sql = `select artist_id from raw_artists where artist_name=?`
+    database.query(sql,[req.params.artist_name],(err,results) => {
+
+        if (err) return console.log(err.message)
+        res.send(results)
+    })
+}
+
+
