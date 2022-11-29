e5185f8587d2bc7ac02cc85ec557940ce430478a
HaosenTan <114191317+HaosenTan@users.noreply.github.com>
2022-11-29 12:16:36 -0500

    Create a connection to the database in rds

--- database/index.js
+++ database/index.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql')
 
-// 创建数据库连接对象
+// Create a connection to the database
 const database = mysql.createPool({
   host: 'lab3-music.c3yxv5zktgur.us-east-1.rds.amazonaws.com',
   user: 'root',
-  password: '00000000',
-  database: 'lab4',
+  password: '88888888',
+  database: 'lab3_music',
   multipleStatements: true,
   //Character encoding format
     charset: 'utf8mb4'
