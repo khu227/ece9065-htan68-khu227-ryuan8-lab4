48055326a617d9cf60b21efe2557d5b5b174a6c9
HaosenTan <114191317+HaosenTan@users.noreply.github.com>
2022-11-29 23:46:41 -0500

    Ability to mark a review as hidden for backend

--- router_handler/admin.js
+++ router_handler/admin.js
@@ -67,7 +67,21 @@ exports.reactivateUser = (req, res) => {
         })
     })
 }
-
+// Ability to mark a review as hidden
+exports.hideReview = (req, res) => {
+    //Determine whether the currently logged-on user is an administrator
+    const sql = `select * from user where id = ? and is_admin =1`
+    database.query(sql, req.user.id, (err, results) => {
+        if (err) return console.log(err.message)
+        if (results.length !== 1) return res.send('You are not an administrator')
+        //Hide one or more reviews
+        const sql = `update review set is_hidden = 1 where id = ?`
+        database .query(sql, req.body.list_id, (err, results) => {
+            if (err) return console.log(err.message)
+            if (results.affectedRows === 0) return res.send('The review does not exist')
+            res.send({status:400,message:'The review was hidden successfully'})
+        } )
+}
 
 
 
