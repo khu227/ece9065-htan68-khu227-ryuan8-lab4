f3bb457ecc1746483fc7015695cee37fd5f8dde6
HaosenTan <114191317+HaosenTan@users.noreply.github.com>
2022-11-30 18:41:48 -0500

    fix err message in register

--- router_handler/user.js
+++ router_handler/user.js
@@ -61,7 +61,7 @@ exports.register = (req, res) => {
                 // Insert the user into the database
                 const sql = `insert into user set ?`
                 database.query(sql, userinfo, (err, results) => {
-                    if (err) return console.log(err.message)
+                    if (err) return send({ status: 401, message: err.message })
                     res.send({ status: 200, message: 'Register successfully, please check your email' })
                 })
                 })
