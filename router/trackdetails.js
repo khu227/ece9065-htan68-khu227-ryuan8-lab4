3d60ca5a208cb8b18a00991162373b246d0f0040
Rongxin Yuan <ryuan8@uwo.ca>
2022-12-04 01:49:25 -0500

    recover useListInfo to able to display authorized user owned play list detail up to 20 and add creator name on display 10 public playlist function. fixed success status number:200

--- router/trackdetails.js
+++ router/trackdetails.js
@@ -11,14 +11,14 @@ router.get('/trackids/:track_title:album_title', trackdetails_handler.getTrackid
 router.get('/musicdatas/:track_title:artist_name:album_title', trackdetails_handler.getMusicData);
 
 // lab4-3 unauthenticated user
-router.get('/api/open/trackDetailsBycombi',trackdetails_handler.getTrackByCombi)
+router.post('/api/open/trackDetailsBycombi',trackdetails_handler.getTrackByCombi)
 router.get('/api/open/tenPublicList', trackdetails_handler.tenPublicList);//get list info
 router.get('/api/open/tenPublicListMore', trackdetails_handler.tenPublicListMore);//allow user to get more track detail saved in list.
 
 
 //lab4-4 authenticated user
 router.post('/api/secure/userNewList', trackdetails_handler.userNewList);
-//router.get('/api/secure/userListInfo', trackdetails_handler.userListInfo);
+router.post('/api/secure/userListInfo', trackdetails_handler.userListInfo);
 router.post('/api/secure/newPlayListAspects', trackdetails_handler.newPlayListAspects);
 router.post('/api/secure/delExitList', trackdetails_handler.delExitList);
 module.exports = router;
